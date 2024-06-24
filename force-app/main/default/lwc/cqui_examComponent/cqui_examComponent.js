95b3554b3c340560e0f238ab3325ba68873b5f2a
RohitBajra <104704874+rohitnewgit@users.noreply.github.com>
2024-06-24 10:36:52 +0545

    logic to save candidate answer in object

--- force-app/main/default/lwc/cqui_examComponent/cqui_examComponent.js
+++ force-app/main/default/lwc/cqui_examComponent/cqui_examComponent.js
@@ -1,56 +1,57 @@
-
-import { LightningElement, wire, track } from 'lwc';
 import getAssignedQuestions from '@salesforce/apex/SQX_examController.getAssignedQuestions';
+import { LightningElement, track, wire } from 'lwc';
 
 export default class ExamComponent extends LightningElement {
     @track exams = [];
     @track error;
-
-    // connectedCallback() {
-    //     const savedStartTime = localStorage.getItem('startTime');
-    //     if (savedStartTime) {
-    //         this.startTimer();
-           
-    //     }
-
-    // }
+    @track userAnswers = [];
 
     @wire(getAssignedQuestions)
     wiredExams({ error, data }) {
-        console.log('data:::'+JSON.stringify(data));
+        console.log('data:::' + JSON.stringify(data));
         if (data) {
-            this.exams = data.map(exam => {
+            this.exams = data.map((exam, idx) => {
                 return {
                     ...exam,
-                    Question_Title:cleanQuestionString(exam.Question_Title),
+                    Question_Title: cleanQuestionString(exam.Question_Title),
                     isMCQ: exam.Question_Type === 'MCQ',
                     isMultiple_Select_MCQ: exam.Question_Type === 'Multiple Select MCQ',
                     isFreeEnd: exam.Question_Type === 'Free End',
                     questionOptions: exam.Question_Options ? exam.Question_Options.split('/') : [], // Split options if they are in a string format
-                    selectedOptions: [] // Initialize selected options array
+                    selectedOptions: [], // Initialize selected options array
+                    number: idx + 1 // Add the question number
                 };
             });
+
             this.error = undefined;
         } else if (error) {
             this.error = error;
             this.exams = [];
         }
     }
-    
+
     handleOptionChange(event) {
+        const questionNumber = parseInt(event.target.dataset.number, 10);
         const questionId = event.target.dataset.id;
-        const option = event.target.dataset.option;
+        const option = event.target.value;
         const isChecked = event.target.checked;
-    
-        // Update the selected options for the specific question
+
         this.exams = this.exams.map(exam => {
-            if (exam.Id === questionId) {
-                let selectedOptions = exam.selectedOptions.slice(); // Create a copy of the array
+            if (exam.number === questionNumber) {
+                let selectedOptions = exam.isMultiple_Select_MCQ ? exam.selectedOptions.slice() : []; // For single-select MCQs, reset selected options
                 if (isChecked) {
-                    selectedOptions.push(option); // Add the selected option
+                    if (exam.isMultiple_Select_MCQ) {
+                        selectedOptions.push(option);
+                    } else {
+                        selectedOptions = [option]; // For single-select MCQs, replace the entire array with the selected option
+                    }
                 } else {
-                    selectedOptions = selectedOptions.filter(item => item !== option); // Remove the deselected option
+                    selectedOptions = selectedOptions.filter(item => item !== option);
                 }
+                if (exam.isMultiple_Select_MCQ) {
+                    selectedOptions = this.sortOptionsByUIOrder(exam.questionOptions, selectedOptions);
+                }
+                this.updateUserAnswers(questionId, questionNumber, selectedOptions);
                 return { ...exam, selectedOptions };
             }
             return exam;
@@ -58,23 +59,44 @@ export default class ExamComponent extends LightningElement {
     }
 
     handleAnswerChange(event) {
+        const questionNumber = parseInt(event.target.dataset.number, 10);
         const questionId = event.target.dataset.id;
         const answer = event.target.value;
 
-        // Update the userAnswer for the specific question
+        this.updateUserAnswers(questionId, questionNumber, answer);
+
         this.exams = this.exams.map(exam => {
-            if (exam.Id === questionId) {
+            if (exam.number === questionNumber) {
                 return { ...exam, userAnswer: answer };
             }
             return exam;
         });
     }
+
+    updateUserAnswers(questionId, questionNumber, answer) {
+        const existingAnswerIndex = this.userAnswers.findIndex(ans => ans.questionNumber === questionNumber);
+        if (existingAnswerIndex > -1) {
+            this.userAnswers[existingAnswerIndex].answer = answer;
+        } else {
+            this.userAnswers.push({ questionId, questionNumber, answer });
+        }
+
+        // Sort userAnswers by questionNumber
+        this.userAnswers.sort((a, b) => a.questionNumber - b.questionNumber);
+
+        console.log('Updated userAnswers:', JSON.stringify(this.userAnswers));
+    }
+
+    sortOptionsByUIOrder(originalOptions, selectedOptions) {
+        return selectedOptions.sort((a, b) => originalOptions.indexOf(a) - originalOptions.indexOf(b));
+    }
+
     get numberedExams() {
         if (!this.exams) return [];
-        return this.exams.map((exam, idx) => ({ ...exam, number: idx + 1 }));
-      }
+        return this.exams;
+    }
 }
-// removes html tags, convert &quot into double quotation   
+
 function cleanQuestionString(question) {
     const tempElement = document.createElement('div');
     tempElement.innerHTML = question;
