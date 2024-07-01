91b9f1948e8c5f442699b5a9ba61922918cd04cd
ATHEX7 <anjan629@gmail.com>
2024-07-01 10:55:53 +0545

    Problem of styling sloved

--- force-app/main/default/lwc/cqui_candidateResult/cqui_candidateResult.js
+++ force-app/main/default/lwc/cqui_candidateResult/cqui_candidateResult.js
@@ -15,14 +15,23 @@ export default class ExamComponent extends LightningElement {
     fullMarks = '';
     passMarks = '';
     @api recordId;
+    
     connectedCallback() {
-       
-        getAssignedQuestions({recordId:this.recordId})
+        this.loadExamData()
+            .then(() => this.loadCandidateResponse())
+            .then(() => this.updateAnswerStyles())
+            .catch(error => {
+                console.error("Error in connectedCallback:: " + JSON.stringify(error));
+                this.error = error;
+            });
+    }
+
+    loadExamData() {
+        return getAssignedQuestions({ recordId: this.recordId })
             .then(result => {
                 this.setName = result[0].Set_Name;
                 this.fullMarks = result[0].Full_Marks;
                 this.passMarks = result[0].Pass_Marks;
-                // console.log("result::: "+JSON.stringify(result));
                 if (result && result.length > 0) {
                     this.exams = result.map((exam, idx) => {
                         const questionOptions = exam.Question_Options ? exam.Question_Options.split('/') : [];
@@ -50,29 +59,23 @@ export default class ExamComponent extends LightningElement {
                 }
             })
             .catch(error => {
-                console.error("error fetching questions:: "+JSON.stringify(error));
+                console.error("Error fetching questions:: " + JSON.stringify(error));
                 this.error = error;
                 this.exams = [];
             });
-            this.loadCandidateResponse();
     }
-
 
     loadCandidateResponse() {
-        console.log("candidate:: "+JSON.stringify(this.exams));
-        getCandidateResponse({ recordId: this.recordId })
+        return getCandidateResponse({ recordId: this.recordId })
             .then(result => {
-                console.log("candidate result::: "+JSON.stringify(result));
+                console.log("Candidate result::: " + JSON.stringify(result));
                 const parsedResult = JSON.parse(result);
                 this.userAnswers = parsedResult;
-                this.updateAnswerStyles();
             })
             .catch(error => {
-                console.error("Error fetching candidate Response:: "+JSON.stringify(error));
+                console.error("Error fetching candidate response:: " + JSON.stringify(error));
             });
     }
-
-
 
     getExamOptionChecked(exam, option) {
         return exam.selectedOption === option.value;
@@ -83,33 +86,24 @@ export default class ExamComponent extends LightningElement {
     }
 
     updateAnswerStyles() {
-        // First, map the user answers to question IDs for easy lookup
         const userAnswersMap = this.userAnswers.reduce((map, userAnswer) => {
             map[userAnswer.questionNumber] = userAnswer.answer;
             return map;
         }, {});
         console.log("userAnswersMap:::" + JSON.stringify(userAnswersMap));
     
-        // Filter the exams array to include only exams that have user answers
         const filteredExams = this.exams.filter(exam => userAnswersMap.hasOwnProperty(exam.number));
     
-        // Update the exams array with proper option classes and user answers
         this.exams = filteredExams.map(exam => {
             const userAnswer = userAnswersMap[exam.number];
             console.log("userAnswer:::" + JSON.stringify(userAnswer));
     
             if (exam.isMCQ || exam.isMultiple_Select_MCQ) {
-                // Update options with classes for MCQ and Multiple Select MCQ
                 exam.questionOptions = exam.questionOptions.map(option => {
-                    // Determine if this option is the correct answer
                     const isCorrect = exam.correctAnswer.includes(option.label);
-    
-                    // Determine if the user selected this option
                     const isSelected = exam.isMCQ
                         ? userAnswer === option.label
-                        : userAnswer.split(', ').includes(option.label); // Handle multiple selections
-    
-                    // Determine the option class based on selection and correctness
+                        : userAnswer.split(', ').includes(option.label);
                     let optionClass = 'default-option';
                     if (isSelected) {
                         optionClass = isCorrect ? 'correct-answer' : 'incorrect-answer';
@@ -118,7 +112,6 @@ export default class ExamComponent extends LightningElement {
                     } else if (isCorrect) {
                         optionClass = 'correct-answer';
                     }
-    
                     return {
                         ...option,
                         optionClass
@@ -131,9 +124,7 @@ export default class ExamComponent extends LightningElement {
             return exam;
         });
     }
-    
 
- 
     get numberedExams() {
         return this.exams;
     }
